24d90b7e22db6d70c125f4e2e050c01822790e28
Justina Mandaro <bautistajustina7@gmail.com>
2022-07-23 11:20:15 -0700

    added click game button

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-<button onclick="document.location='default.asp'">HTML Tutorial</button>
+const clickGameButton =document.getElementById("clickGameButton");
+
+function moveButton() {
+    const buttonPosition1 = clickGameButton.style.left.replace("px","");
+    const buttonPosition2 = clickGameButton.style.top.replace("px","");
+    const left = parseInt(buttonPosition1, 10);
+    const top = parseInt(buttonPosition2, 10);
+
+    clickGameButton.style.left = `${Math.random()*1000}px`;
+    clickGameButton.style.top = `${Math.random()*1000}px`;
+  
+}
+
+
+document.addEventListener("click", function () {
+    moveButton();
+})
